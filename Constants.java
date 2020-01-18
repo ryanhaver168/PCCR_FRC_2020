506ca34964fea5b50a9dd01549c44d9ddf0118f6
CentralCatholic2641 <59667023+CentralCatholic2641@users.noreply.github.com>
2020-01-18 17:20:46 -0500

    Constant Changes

--- Constants.java
+++ Constants.java
@@ -16,17 +16,24 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants{
+    //for timed Robot
+    public static double moveUntilSeconds = 10;
+    public static double moveWithSpeed = .75;
 
-    public static int leftMotor1 = 4;
+    //Talon COnfiguration
+	public static int leftMotor1 = 4;
     public static int leftMotor2 = 5;
     public static int leftMotor3 = 6;
     public static int rightMotor1 = 1;
     public static int rightMotor2 = 2;
     public static int rightMotor3 = 3;
 
+
+    //The GamepadCOntroller
     public static int gamepad1Port = 0;
     public static int joystickPort = 1;
     public static int joystickPort2 = 5;
+    public static int xButtonPort = 1;
  
 
 
